8282d5199509ad8213516f4628e58a21a444ff2b
Gocha64 <70147992+Gocha64@users.noreply.github.com>
2022-03-29 17:22:37 +0900

    prime_factorization

---
+++ basic_math2/right_triangle_4153.py
@@ -0,0 +1,14 @@
+
+triList = []
+while(True):
+    a, b, c = map(int, input().split())
+    if a == 0 and b == 0 and c == 0:
+        break
+    triList.append(sorted([a,b,c]))
+
+for i in triList:
+    a, b, c = map(pow, i, [2]*len(i))
+    if c == (a + b):
+        print("right")
+    else:
+        print("wrong")
