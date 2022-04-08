82a7e7f929678634b4e9750ed67aee2ccc5c5d28
Gocha64 <70147992+Gocha64@users.noreply.github.com>
2022-04-08 21:33:44 +0900

    fibonacci5

---
+++ recursive/fibonacci5_10870.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+
+#재귀를 사용한 방법
+def fibonacchi(n):
+
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+
+    return fibonacchi(n-1) + fibonacchi(n-2)
+
+print(fibonacchi(n))
